e92cc776fa8250b1534b092fca9cc341d23ce5e3
Alex Babichev <alex.babichev@me.com>
2020-01-23 16:30:22 +0100

    _

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Suspense } from 'react';
 import { HashRouter as Router, Route } from "react-router-dom";
 
 import { Header } from '../components/app-header/header.component';
@@ -10,20 +10,57 @@ import { Product } from './product/product.page';
 import { Cases } from './cases/cases.page';
 import { Contact } from './contact/contact.page';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimesCircle, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+
+const notify = localStorage.getItem('notify');
+
 function App() {
+  const i18n = null;
+  const [shouldShowNotify, hideNotify] = useState(!notify);
+
+  const onCloseNotify = () => {
+    localStorage.setItem('notify', '1');
+    hideNotify(false);
+  }
+
   return (
-    <section className="App">
-      <Router>
-        <Header />
-        <Route exact path="/" component={Home} />
-        <Route path="/searchByIdStructured/:id" component={Result}/>
-        <Route path="/searchByIdNonStructured/:id" component={Result}/>
-        <Route exact path="/product" component={Product} />
-        <Route exact path="/cases" component={Cases} />
-        <Route exact path="/contact" component={Contact} />
-        <Footer />
-      </Router>
-    </section>
+    <Suspense fallback="loading">
+      <section className={'AppNotification ' + (shouldShowNotify ? 'active' : '')}>
+        <div className="content">
+          <div className="contentInner">
+            <span className="exclamatione">
+              <FontAwesomeIcon className="" icon={faExclamationTriangle} />
+            </span>
+            <span>
+              <span>
+                Cronica will never ask you for personal or financial information.
+                Make sure the web address in your browser is <b>verify.bankaletihad.com</b> and that you're using a secure connection.
+              </span>
+                <br />
+              <span className="rtl">
+                لن تطلب منك Cronica أبداً أن تشارك معلوماتك الشخصية أو المالية. تأكد من أن موقع الويب في المتصفح هو <b>verify.bankaletihad.com</b> وأنك تستخدم إتصالاً آمناً.
+              </span>
+            </span>
+          </div>
+        </div>
+        <div className="close" onClick={onCloseNotify}>
+          <FontAwesomeIcon icon={faTimesCircle} />
+        </div>
+      </section>
+      <section className={'App ' + (i18n && i18n.dir(window.localStorage.i18nextLng || 'en'))}>
+        <Router>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route path="/searchByIdStructured/:id" component={Result} />
+          <Route path="/searchByIdNonStructured/:id" component={Result} />
+          <Route exact path="/product" component={Product} />
+          <Route exact path="/cases" component={Cases} />
+          <Route exact path="/contact" component={Contact} />
+          <Footer />
+        </Router>
+      </section>
+    </Suspense>
   );
 }
 
