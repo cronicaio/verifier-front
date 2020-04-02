755fcf692856a121d155ed94de39d38b04b74b80
Alex Babichev <alex.babichev@me.com>
2020-04-02 12:13:28 +0200

    _

--- src/pages/home/home.page.js
+++ src/pages/home/home.page.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import { VerifyForm } from '../../components/verify-form/verify-form.component';
 
@@ -7,6 +7,10 @@ import './home.page.scss';
 function Home({ match }) {
   const [result, setResult] = useState(null);
   const [executeScroll, scrollHtmlAttributes] = useScroll();
+
+  useEffect(() => {
+    counter();
+  }, [])
 
   return (
     <section className="Page HomePage">
@@ -18,6 +22,12 @@ function Home({ match }) {
         التحقق الفوري من صحة التصريح
         </h4>
         <VerifyForm onFetch={onFetch} params={match.params} />
+
+        <section className="flip-block textCenter">
+          <h2>Now securely storing:</h2>
+          <div class="flip-clock" id="flip"></div>
+          <p>documents in blockchain</p>
+        </section>
       </header>
     </section>
   );
@@ -37,3 +47,78 @@ function Home({ match }) {
 }
 
 export { Home };
+
+
+
+function counter() {
+
+  function CountdownTracker(label, value) {
+    const el = document.createElement('span');
+
+    el.className = 'flip-clock__piece';
+    el.innerHTML = '<b class="flip-clock__card card"><b class="card__top"></b><b class="card__bottom"></b><b class="card__back"><b class="card__bottom"></b></b></b>' +
+      '<span class="flip-clock__slot">' + label + '</span>';
+
+    this.el = el;
+
+    var top = el.querySelector('.card__top'),
+      bottom = el.querySelector('.card__bottom'),
+      back = el.querySelector('.card__back'),
+      backBottom = el.querySelector('.card__back .card__bottom');
+
+    this.update = function (val) {
+      if (val !== this.currentValue) {
+        if (this.currentValue >= 0) {
+          back.setAttribute('data-value', this.currentValue);
+          bottom.setAttribute('data-value', this.currentValue);
+        }
+        this.currentValue = val;
+        top.innerText = this.currentValue;
+        backBottom.setAttribute('data-value', this.currentValue);
+
+        this.el.classList.remove('flip');
+        void this.el.offsetWidth;
+        this.el.classList.add('flip');
+      }
+    }
+
+    this.update(value);
+
+  }
+
+  function initFlip(count) {
+    const el = document.getElementById('flip');
+    const arr = count.split('');
+
+    const trackers = arr.map((v, key) => new CountdownTracker(key, v));
+    trackers.forEach(tracker => el.appendChild(tracker.el));
+
+    function updateFlip(value) {
+      const newarr = value.split('');
+
+      if (newarr.length !== trackers.length) {
+        const newtracker = new CountdownTracker(trackers.length - 1, newarr[newarr.length - 1])
+        trackers.push(newtracker);
+        el.appendChild(newtracker.el)
+      }
+
+      count = value;
+      const arr = count.split('');
+
+      trackers.forEach((tracker, i) => tracker.update(arr[i]))
+    }
+
+    setInterval(() => {
+      getCount().then(r => updateFlip(r));
+    }, 4000);
+  }
+
+  getCount().then(r => initFlip(r));
+
+  function getCount() {
+    return fetch('https://api-issuer.b1.cronica.cloud/v1/documents/')
+      .then(response => response.json())
+      .then(data => data.count + '')
+      .catch(console.log)
+  }
+}
